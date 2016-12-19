fd42dbceec1511a6658c73bac7c371159900e08a
Naki21 <Yianni21@gmail.com>
2016-12-19 15:05:38 -0500

    HOTFIX for "poapi"
    Needed to add token in the headers of our get request for past orders
    Changed api route from "past-orders" to "pastorders"
    Call now works successfully

--- assets/scripts/pastorders/poapi.js
+++ assets/scripts/pastorders/poapi.js
@@ -1,12 +1,15 @@
 'use strict';
-
+const store = require('../store');
 const config = require('../config');
 // const store = require('../store');
 
 const indexPastOrders = () =>
   $.ajax({
-      url: config.host + '/past-orders',
-      method: 'GET'
+      url: config.host + '/pastorders',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + store.user.token,
+      },
   });
 
 module.exports = {
