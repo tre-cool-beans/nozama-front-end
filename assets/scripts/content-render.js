fac8b5de439009d7cdece7e014b7f5b142b8b656
joaufi <johnathan.a.fisher@gmail.com>
2016-12-19 14:04:32 -0500

    Create renderPastOrders
    Created renderPastOrders in content-render to pass the past_orders
    array of data to the index-past-orders handlebar template.
    Next Steps: Link handlers in index.js

--- assets/scripts/content-render.js
+++ assets/scripts/content-render.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const store = require('./store');
+// const store = require('./store');
 
 const indexProducts = require('../templates/index-products.handlebars');
 const showCart = require('../templates/user-cart.handlebars');
+const indexPastOrders = require('../templates/index-past-orders.handlebars');
 
 const renderProducts = (products) => {
   $('#content').html(indexProducts(products));
@@ -15,6 +16,11 @@ const renderCart = (user_data) => {
   $('#content').trigger('show-cart-change');
 };
 
+const renderPastOrders = (past_orders) => {
+  $('#content').html(indexPastOrders(past_orders));
+  $('#content').trigger('show-past-order-change');
+};
+
 // const renderWelcomePage = () => {
 //   $('.hide-till-sign-in').hide();
 //   $('#content').html(showWelcomePageTemplate());
@@ -24,4 +30,5 @@ const renderCart = (user_data) => {
 module.exports = {
   renderProducts,
   renderCart,
+  renderPastOrders,
 };
