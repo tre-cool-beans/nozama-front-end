9a6b227c754b5dfa574b95f2249d5c34e44bbd19
kiraamaa <kira.a.maclean@gmail.com>
2016-12-20 16:01:42 -0500

    Clear forms on successful sign in or sign up
    Input values now clear on successful sign in or sign up.
    Removed test values from sign in input.
    No longer needed.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,11 +5,16 @@ const productEvents = require('../products/pevents');
 
 const success = (data) => {
   $('#messages').text('success');
-  console.log(data);
+  console.log("inside sign up success");
+//   $('#rt1').click(function() {
+//     $('#t1').attr('value', '');
+//     $('#ht1').attr('value', '');
+// });
 };
 
 const changePasswordSuccess = (data) => {
   $('#messages').text('success');
+  $('.modal-input').val('');
   // $('.hidden').hide();
   // $('.hidden').show();
   console.log(data);
@@ -17,13 +22,17 @@ const changePasswordSuccess = (data) => {
 
 const signInSuccess = (data) => {
   store.user = data.user;
+  $('.modal-input').val('');
+  console.log($('.modal-input'));
   $('.nav-list > li').toggleClass('hidden');
   $('.collapse').collapse('hide');
   success(data);
+
 };
 
 const signOutSuccess = () => {
   store.user = null;
+  $('.modal-input').val('');
   console.log('SIGN OUT SUCCESS!');
   $('.nav-list > li').toggleClass('hidden');
   success(store.user);
